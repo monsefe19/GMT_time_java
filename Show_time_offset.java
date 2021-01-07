381557a2c15c7ce6816c08afabaac96b1064a011
monsefe19 <73147104+monsefe19@users.noreply.github.com>
2021-01-07 11:31:11 -0500

    last commite

--- Show_time_offset.java
+++ Show_time_offset.java
@@ -5,9 +5,9 @@ public class Show_time_offset {
         long hour, minut, second, toutalsecond, toutalmint, toutalhour;
         toutalsecond = milisconde/1000;
         second = toutalsecond%60;
-        toutalmint = toutalsecond%(60*60);
-        minut = toutalmint/60;
-        toutalhour = toutalmint/(24*60);
+        toutalmint = toutalsecond/(60);
+        minut = toutalmint%60;
+        toutalhour = toutalmint/(60);
         hour = toutalhour%24;
         System.out.println(hour+"/"+minut+"/"+second);
     }
