9e80382ec016ae9b8c096622f2e704e893eec28d
monsefe19 <73147104+monsefe19@users.noreply.github.com>
2021-01-06 19:38:05 -0500

    java time gmt

---
+++ Show_time_offset.java
@@ -0,0 +1,14 @@
+public class Show_time_offset {
+    public static void main(String[] args) {
+
+        long milisconde = System.currentTimeMillis();
+        long hour, minut, second, toutalsecond, toutalmint, toutalhour;
+        toutalsecond = milisconde/1000;
+        second = toutalsecond%60;
+        toutalmint = toutalsecond%(60*60);
+        minut = toutalmint/60;
+        toutalhour = toutalmint/(24*60);
+        hour = toutalhour%24;
+        System.out.println(hour+"/"+minut+"/"+second);
+    }
+}
